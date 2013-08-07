94168689d1c45f3066318763ac8b701f97468755
Chip Freeney <Chip.Freeney@careerbuilder.com>
2013-08-07 17:12:06 -0400

    added the pretty view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsSoundSpy::Application.routes.draw do
+  get "status/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
