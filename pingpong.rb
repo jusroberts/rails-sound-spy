3117b68c48f614348f2d4b792dfb6a317d13d282
Justin Roberts <justin.roberts@careerbuilder.com>
2013-09-24 11:01:15 -0400

    some minor tweeks

--- pingpong.rb
+++ pingpong.rb
@@ -4,13 +4,13 @@ require 'rubygems'
 require 'wav-file'
 
 #return true if less than time interval
-def recent_detection (last_detection, current_detection)
-  interval = 3000
-  if last_detection.nil?
-    return false
-  end
-  return (current_detection < last_detection + interval)
-end
+#def recent_detection (last_detection, current_detection)
+#  interval = 3000
+#  if last_detection.nil?
+#    return false
+#  end
+#  return (current_detection < last_detection + interval)
+#end
 
 def get_value(rawSound, index)
   return (rawSound[index].unpack('H*')[0].to_i(8) + (rawSound[index + 1].unpack('H*')[0].to_i(8) << 8)).to_f
@@ -21,7 +21,7 @@ def record_audio
 end
 
 def load_audio
-  f =  open("out.wav")
+  f =  open('out.wav')
   format = WavFile::readFormat(f)
   puts format
   puts
@@ -32,19 +32,16 @@ def load_audio
 end
 
 def delete_old_file
-  return system("rm out.wav")
+  return system('rm out.wav')
 end
 
 def detect_pings(rawSound)
-  last_detection = nil
   for index in 0..((rawSound.length - 1) / 2)
     if ((rawSound[index * 2 + 1].unpack('H*')[0].to_i(8) >> 4) > 0)
-      unless recent_detection(last_detection, index * 2)
-        minutes = (index / (60 * 8000)).to_i
-        seconds = ((index.to_f / 8000.0) - (minutes * 60))
-        puts "#{minutes}:#{seconds.round(3)} :::: #{get_value(rawSound,index * 2)} :: #{Time.now}"
-        last_detection = index * 2
-      end
+        puts "#{Time.now} Ping Detected :: Amplitude: #{get_value(rawSound,index * 2)}"
+        system("echo #{Time.now} :: Amplitude: #{get_value(rawSound,index * 2)} >> ~/log/pingpong")
+        #Add DB call here
+        return
     end
   end
 end
@@ -56,6 +53,11 @@ def main_loop
   detect_pings(rawSound)
 end
 
+#Create Log file if none
+system('mkdir ~/log')
+system('touch ~/log/pingpong')
+
+# RUN
 while true
   main_loop
 end
