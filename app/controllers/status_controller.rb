6bf7dd34a0fb7c8e49f696afe8d49052dca21447
Justin Roberts <justin.roberts@careerbuilder.com>
2013-10-16 13:50:31 -0400

    removing history, adding names

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,7 +1,7 @@
 class StatusController < ApplicationController
   def index
   	available
-    chart
+    #chart
   end
 
   def available
@@ -13,57 +13,57 @@ class StatusController < ApplicationController
    		@available = 'unknown'
    	end
   end
-
-  def chart
-
-    @yesterdayChartData = Array.new((9 * 60 / 5), 0)
-    @todayChartData = Array.new((9 * 60 / 5), 0)
-    @averageChartData = Array.new((9 * 60 / 5), 0)
-
-    histories = History.all
-    days = Days.all
-
-    #This is set to today and tomorrow until we get more data.
-    yesterday = Date.yesterday.to_time
-    today = Date.today.to_time
-
-    @day = nil
-    @finalDay = nil
-    @numDays = 0
-
-    startTime = 9 * 60 * 60
-    endTime = 18 * 60 * 60
-
-    histories.each do |h|
-      @averageChartData[h.time] = h.number_of_hits
-    end
-
-    @numDays = days.size
-
-    unless @numDays == 0
-      @averageChartData.map!{
-          |n| n / @numDays
-      }
-    end
-
-    #TODO: add average data points
-    @hitsChart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title({ :text=>"Ping Pong Data"})
-      f.legend({:align => 'right',
-                :x => -100,
-                :verticalAlign=>'top',
-                :y=>20,
-                :floating=>"true",
-                :backgroundColor=>'#FFFFFF',
-                :borderColor=>'#CCC',
-                :borderWidth=>1,
-                :shadow=>"false"
-               })
-      f.options[:yAxis][:title] = {:text=>"Detections"}
-      f.options[:xAxis][:title] = {:text=>"Time"}
-      f.series(:type=> 'spline',:name=> 'Previous', :data=> @averageChartData)
-
-    end
-  end
+  #
+  #def chart
+  #
+  #  @yesterdayChartData = Array.new((9 * 60 / 5), 0)
+  #  @todayChartData = Array.new((9 * 60 / 5), 0)
+  #  @averageChartData = Array.new((9 * 60 / 5), 0)
+  #
+  #  histories = History.all
+  #  days = Days.all
+  #
+  #  #This is set to today and tomorrow until we get more data.
+  #  yesterday = Date.yesterday.to_time
+  #  today = Date.today.to_time
+  #
+  #  @day = nil
+  #  @finalDay = nil
+  #  @numDays = 0
+  #
+  #  startTime = 9 * 60 * 60
+  #  endTime = 18 * 60 * 60
+  #
+  #  histories.each do |h|
+  #    @averageChartData[h.time] = h.number_of_hits
+  #  end
+  #
+  #  @numDays = days.size
+  #
+  #  unless @numDays == 0
+  #    @averageChartData.map!{
+  #        |n| n / @numDays
+  #    }
+  #  end
+  #
+  #  #TODO: add average data points
+  #  @hitsChart = LazyHighCharts::HighChart.new('graph') do |f|
+  #    f.title({ :text=>"Ping Pong Data"})
+  #    f.legend({:align => 'right',
+  #              :x => -100,
+  #              :verticalAlign=>'top',
+  #              :y=>20,
+  #              :floating=>"true",
+  #              :backgroundColor=>'#FFFFFF',
+  #              :borderColor=>'#CCC',
+  #              :borderWidth=>1,
+  #              :shadow=>"false"
+  #             })
+  #    f.options[:yAxis][:title] = {:text=>"Detections"}
+  #    f.options[:xAxis][:title] = {:text=>"Time"}
+  #    f.series(:type=> 'spline',:name=> 'Previous', :data=> @averageChartData)
+  #
+  #  end
+  #end
 
 end
